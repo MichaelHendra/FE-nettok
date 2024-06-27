a89b9c596c39ac7b95d6216b2fd84d752ac1b105
MichaelHendra <michaelhendranata01@gmail.com>
2024-06-27 07:02:36 +0700

    Home Kurang Responsive

--- src/page/home.tsx
+++ src/page/home.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import cssModule from "./Home.module.css"
+import { Link } from "react-router-dom";
 //images
 import Page from "../assets/bg.png"
 import Simbol from "../assets/Netflix_Symbol.png"
 import Play from "../assets/play.png"
 import Info from "../assets/info.png"
-import { Link } from "react-router-dom";
+import Gambar1 from"../assets/gambar1.png"
+import Gambar2 from "../assets/gambar2.png"
+
 
 function Home () {
 return(
@@ -26,6 +29,48 @@ return(
                         {/* <button className={`${cssModule.LineButton} ${cssModule.Button1}`}><img src={Play} alt="play" />Play</button>
                         <div className={cssModule.gap}></div>
                         <button className={`${cssModule.LineButton} ${cssModule.Button2}`}><img src={Info} alt="Info"/>More Info</button> */}
+                    </div>
+                </div>
+            </div>
+            <div className={cssModule.containter}>
+                <div className={cssModule.BgBawah}>
+                    <div className={cssModule.cardLine}>
+                        <p className={`${cssModule.LineText} ${cssModule.textLineBawah}`}>See Again</p>
+                        <div className={cssModule.card}>
+                            <img src={Gambar1} alt="gambar1" className={cssModule.card}/>
+                            <img src={Gambar2} alt="gambar2" className={cssModule.card}/>
+                            <img src={Gambar2} alt="gambar2" className={cssModule.card}/>
+                            <img src={Gambar2} alt="gambar2" className={cssModule.card}/>
+                            <img src={Gambar2} alt="gambar2" className={cssModule.card}/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={cssModule.containter}>
+                <div className={cssModule.BgBawah}>
+                    <div className={cssModule.cardLine}>
+                        <p className={`${cssModule.LineText} ${cssModule.textLineBawah}`}>See Again</p>
+                        <div className={cssModule.card}>
+                            <img src={Gambar1} alt="gambar1" className={cssModule.card}/>
+                            <img src={Gambar2} alt="gambar2" className={cssModule.card}/>
+                            <img src={Gambar2} alt="gambar2" className={cssModule.card}/>
+                            <img src={Gambar2} alt="gambar2" className={cssModule.card}/>
+                            <img src={Gambar2} alt="gambar2" className={cssModule.card}/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={cssModule.containter}>
+                <div className={cssModule.BgBawah}>
+                    <div className={cssModule.cardLine}>
+                        <p className={`${cssModule.LineText} ${cssModule.textLineBawah}`}>See Again</p>
+                        <div className={cssModule.card}>
+                            <img src={Gambar1} alt="gambar1" className={cssModule.card}/>
+                            <img src={Gambar2} alt="gambar2" className={cssModule.card}/>
+                            <img src={Gambar2} alt="gambar2" className={cssModule.card}/>
+                            <img src={Gambar2} alt="gambar2" className={cssModule.card}/>
+                            <img src={Gambar2} alt="gambar2" className={cssModule.card}/>
+                        </div>
                     </div>
                 </div>
             </div>
