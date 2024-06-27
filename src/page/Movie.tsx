5a3f9b753f46b5fc0d3464dbe0b7a7344d71919b
MichaelHendra <michaelhendranata01@gmail.com>
2024-06-27 15:58:52 +0700

    Movie Page

--- src/page/Movie.tsx
+++ src/page/Movie.tsx
@@ -1,9 +1,52 @@
 import React from "react";
+import cssModule from "./Movie.module.css"
+//Image
+import Gambar from "../assets/gambar1.png"
 
 function Movies(){
     return(
-        <div>
-            <h1>Movie</h1>
+        <div className={cssModule.BG}>
+            <div>
+                <div>
+                    <h1 className={cssModule.h1}>Movie</h1>
+                </div>
+                <div>
+                    <div className={cssModule.cardContainer}>
+                        <div className={cssModule.cardMovie}>
+                            <img src={Gambar} alt="Image"/>
+                            <div className={cssModule.info}>
+                                <div className={cssModule.title}>Kirik</div>
+                                <div className={cssModule.details}>Kirik Ireng</div>
+                            </div>
+                            <div className={cssModule.duration}>1:40:1</div>
+                        </div>
+                        <div className={cssModule.cardMovie}>
+                            <img src={Gambar} alt="Image"/>
+                            <div className={cssModule.info}>
+                                <div className={cssModule.title}>Kirik</div>
+                                <div className={cssModule.details}>Kirik Ireng</div>
+                            </div>
+                            <div className={cssModule.duration}>1:40:1</div>
+                        </div>
+                        <div className={cssModule.cardMovie}>
+                            <img src={Gambar} alt="Image"/>
+                            <div className={cssModule.info}>
+                                <div className={cssModule.title}>Kirik</div>
+                                <div className={cssModule.details}>Kirik Ireng</div>
+                            </div>
+                            <div className={cssModule.duration}>1:40:1</div>
+                        </div>
+                        <div className={cssModule.cardMovie}>
+                            <img src={Gambar} alt="Image"/>
+                            <div className={cssModule.info}>
+                                <div className={cssModule.title}>Kirik</div>
+                                <div className={cssModule.details}>Kirik Ireng</div>
+                            </div>
+                            <div className={cssModule.duration}>1:40:1</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
